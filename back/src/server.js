8cc0be4e64556c8bddb70df8ad23e8b06ba37d0c
Nurlana Quluyeva <81523883+nurlana-qlyva@users.noreply.github.com>
2025-11-10 17:34:59 +0300

    prepared for development

--- back/src/server.js
+++ back/src/server.js
@@ -3,17 +3,36 @@ import dotenv from "dotenv";
 import notesRouter from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
 app.use("/api/notes", notesRouter);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../front/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../front", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(port, () => {
